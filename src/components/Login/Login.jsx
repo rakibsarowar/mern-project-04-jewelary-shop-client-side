7966e2e60828c3cac3df24d25b2cb014a5aa418f
rakibsarowar <rakibsarowar@gmail.com>
2023-10-30 21:27:27 +0600

    working on login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,9 @@
 import React, { useContext, useState } from 'react';
-import { signOut } from 'firebase/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { authContext } from '../AuthProvider/AuthProvider';
+import image1 from "../../assets/Login2.png"
+import "./Login.css"
+
 
 const Login = () => {
     const { signIn, user, signInWithGoogle } = useContext(authContext);
@@ -49,47 +51,46 @@ const Login = () => {
 
     return (
         <div>
-            <div className="hero min-h-screen">
-                <div className="w-full h-screen flex-col bg-[#3e3d52]">
+            <div className="w-full h-screen flex flex-col justify-center bg-[#3e3d52]">
 
-                    <div className="card rounded-none w-2/6 flex-shrink-0 mx-auto shadow-2xl bg-[#343242]">
-                        <div className="card-body">
-                            <div className="text-center">
-                                <h1 className="text-5xl font-bold text-white">Login now!</h1>
-
-                            </div>
-                            <form onSubmit={handleLogin}>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Email Address</span>
-                                    </label>
-                                    <input type="text" name='email' placeholder="email" className="input input-bordered" required />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Password</span>
-                                    </label>
-                                    <input type="text" name='password' placeholder="password" className="input input-bordered" required />
-                                    <label className="label">
-                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                    </label>
-                                </div>
-                                <div className="form-control mt-6">
-                                    <button className="btn btn-primary">Login</button>
-                                </div>
-                            </form>
-
-                        </div>
-                        <div className='text-center'>
-                            Or,
-                        </div>
-                        <button onClick={handleGoogleSignIn} className="btn btn-primary m-4">Google</button>
+                <div className="card rounded-none w-2/6 flex-shrink-0 mx-auto shadow-2xl bg-[#343242]">
+                    <div className='diamond'>
+                    <img src={image1} alt="" />
+                    </div>
+                    <div className="card-body">
                         <div className="text-center">
-                            <h1 className="p-4">New member? <Link className='font-bold underline' to="/register">Register</Link> here</h1>
+                            <h1 className="text-5xl font-bold text-white">Login now!</h1>
                         </div>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-white">Email Address</span>
+                                </label>
+                                <input type="text" name='email' placeholder="email" className="input input-bordered rounded-none" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-white">Password</span>
+                                </label>
+                                <input type="text" name='password' placeholder="password" className="input input-bordered rounded-none" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+
+                    </div>
+                    <div className='text-center'>
+                        Or,
+                    </div>
+                    <button onClick={handleGoogleSignIn} className="btn btn-primary m-4">Google</button>
+                    <div className="text-center">
+                        <h1 className="p-4">New member? <Link className='font-bold underline' to="/register">Register</Link> here</h1>
                     </div>
                 </div>
-
             </div>
         </div>
     );
